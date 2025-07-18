6ffca67586ac5f5c0907732a2bf21121e23fa010
mintbanshee <alexandria.mcqueen@triosstudent.com>
2025-07-18 18:17:04 -0400

    Question 218

---
+++ Ex218.java
@@ -0,0 +1,16 @@
+public class Ex218 {
+  public static void main(String[] args) {
+    // one println 
+    System.out.println("*********     ***        *        ");
+    System.out.println("*       *   *     *     ***       ");
+    System.out.println("*       *  *       *   *****      ");
+    System.out.println("*       *  *       *     *        ");
+    System.out.println("*       *  *       *     *        ");
+    System.out.println("*       *  *       *     *        ");
+    System.out.println("*       *  *       *     *        ");
+    System.out.println("*       *   *     *      *        ");
+    System.out.println("*********     ***        *        ");
+
+
+  } // main
+} // class
