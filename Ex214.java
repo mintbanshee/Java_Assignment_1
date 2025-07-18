66d55d4e952edd058cedd12702eba08a5ac5936a
mintbanshee <alexandria.mcqueen@triosstudent.com>
2025-07-17 21:01:11 -0400

    Question 214

---
+++ Ex214.java
@@ -0,0 +1,17 @@
+public class Ex214 {
+  public static void main(String[] args) {
+    // one println 
+    System.out.println("1 2 3 4");
+
+    // four print
+    System.out.print("1 ");
+    System.out.print("2 ");
+    System.out.print("3 ");
+    System.out.print("4");
+    System.out.println();
+
+    // one printf
+    System.out.printf("%d %d %d %d %n", 1, 2, 3, 4);
+
+  } // main
+} // class
