658c70457f9eb5b4e866813b4c050abc667b5950
mintbanshee <alexandria.mcqueen@triosstudent.com>
2025-07-20 13:44:54 -0400

    Question 433

---
+++ Ex433.java
@@ -0,0 +1,17 @@
+public class Ex433 {
+
+  public static void main(String[] args) {
+
+for (int row = 0; row < 8; row++) {
+  if (row % 2 != 0) {
+    System.out.print(" ");
+  }
+for (int col = 0; col < 8; col++) {
+  System.out.print("* ");
+}
+System.out.println();
+}
+
+  } // close main
+} // close class
+
